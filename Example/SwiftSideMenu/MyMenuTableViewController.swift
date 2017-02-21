892a028c020c3dc9fe9df3830f70865121663430
Jorge Perales <jorge@axented.com>
2017-02-20 18:06:28 -0600

    Fix original menu

--- Example/SwiftSideMenu/MyMenuTableViewController.swift
+++ Example/SwiftSideMenu/MyMenuTableViewController.swift
@@ -15,14 +15,14 @@ class MyMenuTableViewController: UITableViewController {
         
         // Customize apperance of table view
         tableView.contentInset = UIEdgeInsetsMake(64.0, 0, 0, 0) //
-        tableView.separatorStyle = .None
-        tableView.backgroundColor = UIColor.clearColor()
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = UIColor.clear
         tableView.scrollsToTop = false
         
         // Preserve selection between presentations
         self.clearsSelectionOnViewWillAppear = false
-        
-        tableView.selectRowAtIndexPath(NSIndexPath(forRow: selectedMenuItem, inSection: 0), animated: false, scrollPosition: .Middle)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CELL")
+        //tableView.selectRow(at: NSIndexPath(row: selectedMenuItem, section: 0) as IndexPath, animated: false, scrollPosition: .middle)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,39 +32,39 @@ class MyMenuTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // Return the number of sections.
         return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
         return 4
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        var cell = tableView.dequeueReusableCellWithIdentifier("CELL")
+        var cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath as IndexPath)
         
         if (cell == nil) {
-            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "CELL")
-            cell!.backgroundColor = UIColor.clearColor()
-            cell!.textLabel?.textColor = UIColor.darkGrayColor()
-            let selectedBackgroundView = UIView(frame: CGRectMake(0, 0, cell!.frame.size.width, cell!.frame.size.height))
-            selectedBackgroundView.backgroundColor = UIColor.grayColor().colorWithAlphaComponent(0.2)
-            cell!.selectedBackgroundView = selectedBackgroundView
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "CELL")
+            cell.backgroundColor = UIColor.clear
+            cell.textLabel?.textColor = UIColor.darkGray
+            let selectedBackgroundView = UIView(frame: CGRect(x: 0, y: 0, width: cell.frame.size.width, height: cell.frame.size.height))
+            selectedBackgroundView.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
+            cell.selectedBackgroundView = selectedBackgroundView
         }
         
-        cell!.textLabel?.text = "ViewController #\(indexPath.row+1)"
+        cell.textLabel?.text = "ViewController #\(indexPath.row+1)"
         
-        return cell!
+        return cell
     }
     
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 50.0
     }
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         print("did select row: \(indexPath.row)")
         
@@ -79,30 +79,20 @@ class MyMenuTableViewController: UITableViewController {
         var destViewController : UIViewController
         switch (indexPath.row) {
         case 0:
-            destViewController = mainStoryboard.instantiateViewControllerWithIdentifier("ViewController1") 
+            destViewController = mainStoryboard.instantiateViewController(withIdentifier: "ViewController1") 
             break
         case 1:
-            destViewController = mainStoryboard.instantiateViewControllerWithIdentifier("ViewController2")
+            destViewController = mainStoryboard.instantiateViewController(withIdentifier: "ViewController2")
             break
         case 2:
-            destViewController = mainStoryboard.instantiateViewControllerWithIdentifier("ViewController3")
+            destViewController = mainStoryboard.instantiateViewController(withIdentifier: "ViewController3")
             break
         default:
-            destViewController = mainStoryboard.instantiateViewControllerWithIdentifier("ViewController4") 
+            destViewController = mainStoryboard.instantiateViewController(withIdentifier: "ViewController4") 
             break
         }
         sideMenuController()?.setContentViewController(destViewController)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
